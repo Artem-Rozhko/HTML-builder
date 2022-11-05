775bafe7b49270bd9c3f064adb59de5c7e64d914
Artem Rozhko <artem1996ecm@mail.ru>
2022-11-05 19:10:14 +0300

    add: solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.join(__dirname, 'text.txt')
+const stream = fs.createReadStream(pathFile, 'utf-8');
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message))
