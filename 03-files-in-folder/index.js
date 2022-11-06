3488a4cf083aa0dd7f9a4bfa2d6b90336030ac59
Artem Rozhko <artem1996ecm@mail.ru>
2022-11-06 21:19:58 +0300

    add: solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { stdin, stdout } = process;
+
+(async () => {
+  const pathFolder = path.join(__dirname, 'secret-folder');
+  try {
+    const files = await fs.readdir(pathFolder, { withFileTypes: true });
+    for (const file of files)
+      if (file.isFile()) {
+        const pathFile = path.join(pathFolder, file.name);
+        const fileName = path.parse(pathFile).name;
+        const fileExtension = path.extname(pathFile).slice(1);
+        const fileSize = (await fs.stat(pathFile)).size;
+        stdout.write(`${fileName} - ${fileExtension} - ${fileSize / 1024}kb\n`);
+      }
+  } catch (err) {
+    stdout.write(err);
+  }
+})();
