442498971b3757f87e92c5ca6eb1a266e6926fb4
Artem Rozhko <artem1996ecm@mail.ru>
2022-11-07 23:18:02 +0300

    add: function copyAssets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const fsProm = require('fs/promises');
 const path = require('path');
 const { stdin, stdout } = process;
+const pathFolder = path.join(__dirname, 'assets');
+const pathCopyFolder = path.join(__dirname, 'project-dist', 'assets');
 
-async function creadStyle() {
+async function creatStyle() {
   const pathFilesCss = path.join(__dirname, 'styles');
   const pathProject = path.join(__dirname, 'project-dist');
   const pathCommonFile = path.join(__dirname, 'project-dist', 'style.css');
@@ -24,9 +26,30 @@ async function creadStyle() {
   }
 };
 
+async function copyAssets(originFolder, copyFolder) {
+  try {
+    await fsProm.rm(copyFolder, { recursive: true, force: true })
+    await fsProm.mkdir(copyFolder, { recursive: true })
+    const files = await fsProm.readdir(originFolder, { withFileTypes: true });
+    for (const file of files) {
+      const origin = path.join(originFolder, file.name);
+      const copy = path.join(copyFolder, file.name);
+      if (file.isFile()) {
+        await fsProm.copyFile(origin, copy);
+      } else {
+        await copyAssets(origin, copy);
+      }
+    }
+  } catch (err) {
+    stdout.write(err);
+  }
+};
+
+
 (async () => {
   try {
-    await creadStyle();
+    await creatStyle();
+    await copyAssets(pathFolder, pathCopyFolder);
   } catch (err) {
     console.log(err);
   }
